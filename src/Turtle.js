cd2686271d2367487210494162bec6265fbbdbb8
Luke Cordova <lukethegreat000@gmail.com>
2021-12-01 17:00:10 -0800

    restarting the turtle class

--- src/Turtle.js
+++ src/Turtle.js
@@ -2,113 +2,23 @@ import {default as V} from './Vector.js';
 
 
 export default class Turtle {
-    #posVec;
-    #velocity;
-    #acceleration;
-    #speed;
-    #forces = [];
-    #degreesRotated = 0;
-    #accelerant;
-    constructor(options){
-        let ogo = {
-            x: 0, 
-            y: 0,
-            speed: 5,
-            accelerant: 1
+    constructor(x, y){
+        this.position = V.createNew(x, y);
+        this.velocity = V.createNew(0, 0);
+        this.acceleration = V.createNew(0, 0);
+    }
+    move(){
+        this.position.add(this.velocity);
+    }
+    addForce(...vector){
+        for(let i = 0;i<arguments.length;i++){
+            let cVector = arguments[i].clone();
+            this.acceleration.add(cVector);
         }
-        Object.assign(ogo, options);
-        this.#accelerant = ogo.accelerant;
-        this.#posVec = V.createNew(ogo.x, ogo.y);
-        this.#speed = ogo.speed;
-        this.#velocity = V.createNew(0, 0);
-        this.#acceleration = V.createNew(ogo.speed, ogo.speed);
+        this.velocity.add(this.acceleration).upperLimit(50).lowerLimit(-50);
     }
-    moveTurtle(){
-        if(this.#acceleration){
-            this.#velocity.add(this.#acceleration);
-        }
-        this.#posVec.add(this.#velocity);
-        // this.#forces = [];
-        return this.#posVec;
+    subForce(vector){
+        let cVector = vector.clone();
+        this.velocity.add(cVector.sub(this.position).normalize());
     }
-    
-    // setters
-    subForce(x, y){
-        this.#acceleration = V.createNew(x, y);
-        this.#acceleration.sub(this.#posVec);
-        this.#acceleration.normalize();
-        this.#acceleration.mult(this.#accelerant);
-    }
-
-
-
-
-    addForce(x, y){
-        this.#velocity.add(V.createNew(x, y));
-        this.#velocity.normalize();
-        this.#velocity.mult(this.#speed);
-    }
-    setVelocity(x, y){
-        this.#velocity = V.createNew(x, y);
-    }
-    // getters
-    getDistanceTo(x, y){ //previously getRadius()
-        return Math.sqrt(Math.pow(x - this.#posVec.x, 2) + Math.pow(y - this.#posVec.y, 2));
-    }
-    getOriginAngleTo(x, y){
-        let ogX = this.getX() - x;
-        let rad = this.getRadius(x, y);
-        return Math.acos(ogX/rad);
-    }
-    getAngleTo(x, y){
-        let ogAngle = this.getOriginAngleTo(x, y);
-        let myAngle = Turtle.angConv(this.#degreesRotated, {to: 'radians'});
-        return ogAngle - myAngle;
-    }
-    getXTo(r, theta){
-        return (r * Math.cos(theta)) - Turtle.angConv(this.#degreesRotated, {to: 'radians'});
-    }
-    getYTo(r, theta){
-        return (r * Math.sin(theta)) - Turtle.angConv(this.#degreesRotated, {to: 'radians'});
-    }
-    getX(){return this.#posVec.x;}
-    getY(){return this.#posVec.y;}
-    getPos(){return this.#posVec;}
-    getSpeed(){return this.#speed;}
-    getVelocity(){return this.#velocity;}
-    getDegreesRotated(){return this.#degreesRotated}
-
-    static angConv(angle, options){
-        let aT = ['degrees', 'radians']
-        let ogo = {
-            to: 'radians'
-        }
-        Object.assign(ogo, options);
-        if(ogo.to === aT[0]){
-            return angle * (180/Math.PI);
-        }else if(ogo.to === aT[1]){
-            return angle * (Math.PI/180);
-        }
-    }
-    static normalize(angle, options){
-        let aT = ['degrees', 'radians']
-        let ogo = {
-            type: 'degrees'
-        }
-        Object.assign(ogo, options);
-        let bound;
-        if(ogo.type === 0 || ogo.type === aT[0]){
-            bound = 360;
-        }else if(ogo.type === 1 || ogo.type === aT[1]){
-            bound = 2 * Math.PI;
-        }else{
-            throw 'incorrect angle type';
-        }
-        while(angle > bound)angle -= bound;
-        while(angle < 0)angle += bound;
-        return angle;
-    }
-
 }
-let turt = new Turtle({x:2, y:1, velocity:[1, 1]});
-turt.moveTurtle();
--- test/index.js
+++ test/index.js
@@ -6,87 +6,65 @@ import BoidSimulation from '../src/BoidSimulation.js';
 
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
-let mouseX = canvas.width/2, mouseY = canvas.height/2;
+let turtle = new Turtle(300, 300);
+let turtle1 = new Turtle(canvas.width-300, canvas.height-300);
+let center = V.createNew(canvas.width/2, canvas.height/2);
+turtle.addForce(V.createNew(20, 10));
+turtle1.addForce(V.createNew(20, -10));
 
-
-let vector = V.createNew(100, 100);
-let origin = V.createNew(mouseX, mouseY);
-
-let ogFill = ctx.fillStyle;
-ctx.fillStyle = 'rgba(0,0,0,1)';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.fillStyle = ogFill;
-let r = 255, g = 0, b = 0;
-
-let color = `rgb(${r}, ${g}, ${b})`;
+ctx.fillRect(0,0,canvas.width, canvas.height);
 
 function loop(){
     clear(ctx);
-    drawVector(vector, origin, `rgb(${r}, ${g}, ${b})`);
-    vector.addAngle(.01);
-    let mag = vector.magnitude;
-    mag += (Math.random() * 30) - 15;
-    vector.normalize();
-    vector.mult(mag);
-    vector.upperLimit(400);
-    vector.lowerLimit(50);
-    if(r === 255){
-        if(b===0){
-            g++;
-        }else{
-            b--;
-        }
-    }
-    if(g===255){
-        if(r===0){
-            b++;
-        }else{
-            r--;
-        }
-    }
-    if(b===255){
-        if(g===0){
-            r++;
-        }else{
-            g--;
-        }
-    }
+    turtle.move();
+    turtle1.move();
+    turtle.subForce(turtle1.position);
+    turtle1.subForce(turtle.position);
+    turtle.subForce(center);
+    turtle1.subForce(center);
 
+    drawTurt(turtle);
+    drawVec(turtle.velocity, turtle.position, 'red');
+    drawTurt(turtle1);
+    drawVec(turtle1.velocity, turtle1.position, 'red');
+    // drawVec(turtle.acceleration, turtle.position, 'yellow');
+    drawStabVec(turtle.position, turtle1.position, 'purple');
+    drawStabVec(turtle1.position, turtle.position, 'purple');
 }
 
-
-setTimeout(() => {
-    setInterval(loop, 1)
-}, 500);
-function drawVector(vector, origin = V.createNew(0, 0), color = 'white'){
-    let ogCopy = origin.clone();
-    ogCopy.add(vector);
+function drawTurt(turtle){
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.arc(turtle.position.x, turtle.position.y, 5, 0, 2*Math.PI);
+    ctx.fill();
+}
+function drawVec(vector, origin, color){
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
     ctx.beginPath();
     ctx.moveTo(origin.x, origin.y);
-    ctx.lineTo(ogCopy.x, ogCopy.y);
+    ctx.lineTo(origin.x + vector.x, origin.y + vector.y);
+    ctx.arc(origin.x + vector.x, origin.y + vector.y, 3, 0, 2*Math.PI);
     ctx.stroke();
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.arc(ogCopy.x, ogCopy.y, 3, 0, 2*Math.PI);
     ctx.fill();
 }
-function drawTurt(turtle, size, color = 'cyan'){
+function drawStabVec(vector, origin, color){
+    ctx.strokeStyle = color;
     ctx.fillStyle = color;
-    ctx.strokeStyle = 'magenta'
     ctx.beginPath();
-    ctx.arc(turtle.getX(), turtle.getY(), size, 0, 2*Math.PI);
+    ctx.moveTo(vector.x, vector.y);
+    ctx.lineTo(origin.x, origin.y);
+    ctx.arc(origin.x, origin.y, 3, 0, 2*Math.PI);
+    ctx.stroke();
     ctx.fill();
 }
 
-function clear(ctx){
+setTimeout(() => {
+    setInterval(loop, 20)
+}, 500);
+function clear(ctx) {
     let ogFill = ctx.fillStyle;
-    ctx.fillStyle = 'rgba(0,0,0,.003)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgba(0,0,0,1)';
+    ctx.fillRect(0,0,canvas.width, canvas.height);
     ctx.fillStyle = ogFill;
 }
-
-// canvas.addEventListener('mousemove', (e) => {
-//     mouseX = e.offsetX;
-//     mouseY = e.offsetY;
-// });

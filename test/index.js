480c404d6b95720b08d4bf0d7ecf8e0ca4e33383
Luke Cordova <lukethegreat000@gmail.com>
2021-12-08 16:57:49 -0800

    found the issue with alignment but havent fixed it. Basically when averaging angles, even though an angle like 350 and 4 are pointing in the same general direction, they average to 177 which is in the opposite direction. Have to find a way to average headings more evenly

--- test/index.js
+++ test/index.js
@@ -23,39 +23,68 @@ for(let i = 0;i<100;i++){
         )
     );
     boids[i].velocity.add(
-        V.createNew(1, 
+        V.createNew(
             // (Math.random() * 2) -1, 
             (Math.random() * 2) -1
         ).normalize().mult(2)
     );
 }
-let bs = new BoidSimulation({
-    flock: boids, 
-    canvas: canvas
-});
+// let bs = new BoidSimulation({
+//     flock: boids, 
+//     canvas: canvas
+// });
 
 
-let boid = new Boid(100, 100, {ctx:ctx})
-boid.velocity.add(V.createNew(1, 0));
-boid.velocity.addAngle(Math.PI);
+let boid = new Boid(100, 50, {ctx:ctx, color: 'green'})
+boid.velocity.add(V.createNew((Math.random() * 2) -1, (Math.random() * 2) -1));
+// boid.velocity.addAngle(Math.PI);
 
-let boid1 = new Boid(100, 150, {ctx:ctx});
-boid1.velocity.add(V.createNew(1, (Math.random() * 2) -1).normalize());
-boid1.velocity.addAngle(Math.PI);
+let boid1 = new Boid(100, 100, {ctx:ctx, color: 'magenta'});
+boid1.velocity.add(V.createNew((Math.random() * 2) -1, (Math.random() * 2) -1).normalize());
+// boid1.velocity.addAngle(Math.PI);
 
-let boid2 = new Boid(100, 50, {ctx:ctx});
-boid2.velocity.add(V.createNew(1, (Math.random() * 2) -1).normalize());
-boid2.velocity.addAngle(Math.PI);
+let boid2 = new Boid(100, 150, {ctx:ctx, color: 'cyan'});
+boid2.velocity.add(V.createNew((Math.random() * 2) -1, (Math.random() * 2) -1).normalize());
+// boid2.velocity.addAngle(Math.PI);
 
 ctx.fillRect(0,0,canvas.width, canvas.height);
 function loop(){
     clear(ctx);
+    boid.drawVision();
+    boid1.drawVision();
+    boid2.drawVision();
+    boid.draw();
+    boid1.draw();
+    boid2.draw();
 
-    bs.loop();
+    let al = boid.velocity.getAngle();
+    if(al<0)al = al+(Math.PI*2);
+    let al1 = boid1.velocity.getAngle();
+    if(al1<0)al1 = al1+(Math.PI*2);
+    let al2 = boid2.velocity.getAngle();
+    if(al2<0)al2 = al2+(Math.PI*2);
+
+    let aal = (al1+al2)/2;
+    let aal1 = (al+al2)/2;
+    let aal2 = (al+al1)/2;
+
+    ctx.font = '30px serif';
+    ctx.fillStyle = 'white';
+    ctx.fillText(Math.round(aal * 180/Math.PI), boid.position.x + 100, boid.position.y);
+    ctx.fillText(': '+Math.round(al * 180/Math.PI), boid.position.x + 200, boid.position.y);
+    ctx.fillText(Math.round(aal1 * 180/Math.PI), boid1.position.x + 100, boid1.position.y);
+    ctx.fillText(': '+Math.round(al1 * 180/Math.PI), boid1.position.x + 200, boid1.position.y);
+    ctx.fillText(Math.round(aal2 * 180/Math.PI), boid2.position.x + 100, boid2.position.y);
+    ctx.fillText(': '+Math.round(al2 * 180/Math.PI), boid2.position.x + 200, boid2.position.y);
+
+    boid.velocity.addAngle(alignment(boid, aal));
+    boid1.velocity.addAngle(alignment(boid1, aal1));
+    boid2.velocity.addAngle(alignment(boid2, aal2));
+    // bs.loop();
 }
 function alignment(boid, heading){
     // if(heading > Math.PI){
-    //     heading -= Math.PI;
+    //     heading -= Math.PI*2;
     // }
     let curDir = boid.velocity.getAngle();
     let diff = heading - curDir;
@@ -67,18 +96,25 @@ function alignment(boid, heading){
         diff -= Math.PI*2;
     }
     direction = diff > 0 ? 1 : diff < 0 ? -1 : 0;
-    return .02 * direction;
+    return .01 * direction;
 }
-
-setTimeout(() => {
-    setInterval(loop, 20);
-}, 500);
+var interval = setInterval(loop, 100);
+// setTimeout(() => {
+    
+// }, 500);
 function clear(ctx) {
     let ogFill = ctx.fillStyle;
-    ctx.fillStyle = 'rgba(0,0,0,.03)';
+    ctx.fillStyle = 'rgba(0,0,0,1)';
     ctx.fillRect(0,0,canvas.width, canvas.height);
     ctx.fillStyle = ogFill;
 }
-
+window.addEventListener('click', () => {
+    if(interval!=null){
+        clearInterval(interval);
+        interval = null;
+    }else{
+        interval = setInterval(loop, 100);
+    }
+})
 
 
